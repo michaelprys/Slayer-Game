7b035a940f1f4e1fbf9ca11e1cd4c59816b42e19
Michael <musha09cz@gmail.com>
2023-02-01 01:45:03 +0100

    determining a winner

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
       enemyHealth: 100,
       surrender: false,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
@@ -20,33 +21,60 @@ const app = Vue.createApp({
       return this.currentRound % 3 !== 0;
     },
   },
+  watch: {
+    // if (this.playerHealth < 0) {
+    //   alert("You lost!");
+    // } else if (this.enemyHealth === 0) {
+    //   alert("You won!");
+    // }
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = "enemy";
+      }
+    },
+    enemyHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Enemy lost
+        this.winner = "player";
+      }
+    },
+  },
   methods: {
     attackEnemy() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.enemyHealth -= attackValue;
       this.receiveDamage();
-      if (this.playerHealth === 0) {
-        alert("You lost!");
-      } else if (this.enemyHealth === 0) {
-        alert("You won!");
-      }
+      this.determineAWinner();
     },
     receiveDamage() {
       const receivedDamageValue = getRandomValue(5, 30);
       this.playerHealth -= receivedDamageValue;
     },
     heal() {
-      const restoredHealth = getRandomValue(10, 20);
-      if (this.playerHealth < 100) {
+      this.currentRound++;
+      const restoredHealth = getRandomValue(10, 35);
+      if (this.playerHealth + restoredHealth > 100) {
+        this.playerHealth = 100;
+      } else {
         this.playerHealth += restoredHealth;
       }
+      this.receiveDamage();
+      this.determineAWinner();
     },
     useUltimate() {
       this.currentRound++;
       const ultimateDamageValue = getRandomValue(25, 50);
       this.enemyHealth -= ultimateDamageValue;
       this.receiveDamage();
+      this.determineAWinner();
     },
     endGame() {
       this.surrender === true;
